f2f2b629a7a989d49858adbc0fe7d24d0ef1a065
licrimovor <mr.licrimovor@yandex.ru>
2025-09-28 09:44:43 +0800

    hw1

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-import pandas as pd
+import numpy
 
-a = pd.DataFrame()
-a[a.to_string().]
+import pandas
+
+
+pandas.DataFrame().to_numpy(numpy.int8)
+import torch
+
+indices = torch.Tensor(1, 1, 128, 128, 128).random_(1, 24)
+# indices.shape => torch.Size([1, 1, 128, 128, 128])
+# indices.dtype => torch.float32
+
+n = 24
+one_hot = torch.nn.functional.one_hot(indices.to(torch.int64), n)
+print(one_hot)
